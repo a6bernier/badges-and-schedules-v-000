689490ded794d39cb9b5cd5c3581fe39f57464f1
Andrew Bernier <a6bernier@gmail.com>
2018-10-05 16:30:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
-
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(names)
-  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
   names.each do |name|
    puts "Hello, my name is #{name}."
   end
